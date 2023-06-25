07c44226cf4f48c5f488b4863a98f49e07df0bfc
samer ghosn <samer_ghosn@hotmail.com>
2023-06-25 09:34:25 +0300

    build fixes

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
